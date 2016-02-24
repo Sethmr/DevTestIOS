484313d28769db49208323c4589ebbabfbcbd94b
Sethmr <seth.rininger@students.fhu.edu>
2016-02-24 12:13:32 -0500

    first commit

---
+++ ForecastService.swift
@@ -0,0 +1,9 @@
+//
+//  ForecastService.swift
+//  DevTest
+//
+//  Created by Seth Rininger on 2/23/16.
+//  Copyright © 2016 Seth Rininger. All rights reserved.
+//
+
+import Foundation
---
+++ NetworkOperation.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkOperation.swift
+//  DevTest
+//
+//  Created by Seth Rininger on 2/23/16.
+//  Copyright © 2016 Seth Rininger. All rights reserved.
+//
+
+import Foundation
